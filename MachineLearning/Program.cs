c033d0085a68dd470c2a4331e21433554171e934
Marina <marina.lucena@gloo.com>
2013-11-11 23:20:32 -0200

    parses feitos

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -100,7 +100,159 @@ namespace MachineLearning
             ct.SaveChanges();
         }
 
-   
+        static void ParseColor()
+        {
+
+            string file = (new StreamReader(Path.Combine(pokefolder, "pokeColor.txt"))).ReadToEnd();
+            var colorGroups = file.Replace('\r', ' ').Split(new string[] { "END" }, StringSplitOptions.None);
+
+            foreach (var group in colorGroups) // iterate over color groups
+            {
+                var lines = group.Split('\n');
+                String color = lines[0];
+
+                for (int i = 1; i < lines.Length; i = i + 3)
+                {
+                    var pokes = regInt.Matches(lines[i]);
+                    foreach (Match pokeMatch in pokes)
+                    {
+                        var pokeId = int.Parse(pokeMatch.Value);
+                        var poke = ct.Pokemons.Where(p => p.PokeId == pokeId).FirstOrDefault(); // Doing a query to find the pokemon with this ID
+                        if (poke != null) // if the pokemons exists, we just set the color!
+                            poke.Color = color;
+                        else // if the variabel is null, then the pokemon doenst exists in the database, let's create it!
+                            ct.Pokemons.Add(new Pokemon() { PokeId = pokeId, Color = color });
+                    }
+                }
+            }
+
+        }
+
+        static void ParseHabitat()
+        {
+            string file = (new StreamReader(Path.Combine(pokefolder, "pokeHabitat.txt"))).ReadToEnd();
+            var habitatGroups = file.Replace('\r', ' ').Split(new string[] { "END" }, StringSplitOptions.None);
+
+            foreach (var group in habitatGroups) // iterate over habitat groups
+            {
+                var lines = group.Split('\n');
+                String habitat = lines[0];
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    if (lines[i][0] == '{')
+                    {
+                        var pokes = regInt.Matches(lines[i]);
+                        foreach (Match pokeMatch in pokes)
+                        {
+                            var pokeId = int.Parse(pokeMatch.Value);
+                            var poke = ct.Pokemons.Where(p => p.PokeId == pokeId).FirstOrDefault(); // Doing a query to find the pokemon with this ID
+                            if (poke != null) // if the pokemons exists, we just set the habitat!
+                                poke.Habitat = habitat;
+                            else // if the variabel is null, then the pokemon doenst exists in the database, let's create it!
+                                ct.Pokemons.Add(new Pokemon() { PokeId = pokeId, Habitat = habitat });
+                        }
+                    }           
+                }
+            }
+
+        }
+
+        static void ParseCatchRate()
+        {
+            string file = (new StreamReader(Path.Combine(pokefolder, "pokeCatch.txt"))).ReadToEnd();
+            var lines = file.Split('\n');
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int pokeId, catchRate;
+
+                if (i % 5 == 0)
+                {
+                    pokeId = int.Parse(lines[i]);
+                    catchRate = int.Parse(lines[i + 3]);
+
+                    var pokeBase = ct.Pokemons.Where(p => p.PokeId == pokeId).FirstOrDefault(); // Doing a query to find the pokemon with this ID
+                    if (pokeBase != null) // if the pokemons exists, we just set the catchRate!
+                        pokeBase.CatchRate = catchRate;
+                    else // if the variabel is null, then the pokemon doenst exists in the database, let's create it!
+                        ct.Pokemons.Add(new Pokemon() { PokeId = pokeId, CatchRate = catchRate });
+                }
+            }
+
+        }
+
+        static void ParseCatchRate()
+        {
+            string file = (new StreamReader(Path.Combine(pokefolder, "pokePerformance.txt"))).ReadToEnd();
+            var lines = file.Split('\n');
+
+            foreach (var line in lines)
+            {
+                var item = line.Split('|');
+
+                int pokeId, speed, power, skill, stamina, jump ;
+
+                pokeId = int.Parse(item[0]);
+                speed = int.Parse(item[2]) ;
+                power = int.Parse(item[3]);
+                skill = int.Parse(item[4]) ;
+                stamina = int.Parse(item[5]);
+                jump = int.Parse(item[6]) ;
+
+                var pokeBase = ct.Pokemons.Where(p => p.PokeId == pokeId).FirstOrDefault(); // Doing a query to find the pokemon with this ID
+                if (pokeBase != null) // if the pokemons exists, we just set the performance!
+                {
+                    pokeBase.Jump = jump;
+                    pokeBase.Power = power;
+                    pokeBase.Skill = skill;
+                    pokeBase.Speed = speed;
+                    pokeBase.Stamina = stamina;
+                }
+                else // if the variabel is null, then the pokemon doenst exists in the database, let's create it!
+                    ct.Pokemons.Add(new Pokemon() { PokeId = pokeId, Jump = jump, Power = power,
+                                                    Skill =skill, Stamina = stamina, Speed = speed});
+
+                
+            }
+        }
+
+        static void ParseAbility()
+        {
+            string file = (new StreamReader(Path.Combine(pokefolder, "pokeAbility.txt"))).ReadToEnd();
+            var lines = file.Split('\n');
+
+            foreach (var line in lines)
+            {
+                var item = line.Split('|');
+                int pokeId;
+                String ability1, ability2, hidden;
+
+                pokeId = int.Parse(item[0]);
+                ability1 = item[2];
+                ability2 = item[3];
+                hidden = item[4];
+
+                var pokeBase = ct.Pokemons.Where(p => p.PokeId == pokeId).FirstOrDefault(); // Doing a query to find the pokemon with this ID
+                if (pokeBase != null) // if the pokemons exists, we just set the ability!
+                {
+                    pokeBase.Ability1 = ability1;
+                    pokeBase.Ability2 = ability2;
+                    pokeBase.Hidden = hidden;
+                }
+                else // if the variabel is null, then the pokemon doenst exists in the database, let's create it!
+                    ct.Pokemons.Add(new Pokemon()
+                    {
+                        PokeId = pokeId,
+                        Ability1 = ability1,
+                        Ability2 = ability2,
+                        Hidden = hidden 
+
+                    });
+
+            }
+
+        }
     }
 
  
--- MachineLearning/model/Pokemon.cs
+++ MachineLearning/model/Pokemon.cs
@@ -9,14 +9,23 @@ using System.ComponentModel.DataAnnotations;
 namespace MachineLearning.model
 {
     public class Pokemon
-    {
-        
+    {       
         public int Id { get; set; }
         public int PokeId { get; set; }
+        public int CatchRate { get; set; }
+        public int Speed { get; set; }
+        public int Power { get; set; }
+        public int Skill { get; set; }
+        public int Stamina { get; set; }
+        public int Jump { get; set; }
         public float Height { get; set; }
         public float Widht { get; set; }
         public float Weight { get; set; }
         public String Color { get; set; }
+        public String Habitat { get; set; }
+        public String Ability1 { get; set; }
+        public String Ability2 { get; set; }
+        public String Hidden { get; set; }
         public PokeType Type { get; set; }
     }
 }
